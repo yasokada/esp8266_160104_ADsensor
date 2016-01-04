f7d5c92f9d66ef69135426fb9da1489b3c91f4aa
Yasuhiko OKADA <yasokada@gmail.com>
2016-01-04 16:28:12 +0900

    ignore wifiConfig.h

---
+++ TemplateWifiConfig.h
@@ -0,0 +1,23 @@
+#ifndef WIFICONFIG_H
+#define WIFICONFIG_H
+
+/*
+This is a template for wifiConfig.h.
+Use your SSID and password.
+
+But, 
+!!!!!! VERY IMPORTANT !!!!!!!
+!!!!!! VERY IMPORTANT !!!!!!!
+!!!!!! VERY IMPORTANT !!!!!!!
+DO NOT PUSH the wifi setting file including your information
+to git repository and others for security reason.
+!!!!!! VERY IMPORTANT !!!!!!!
+!!!!!! VERY IMPORTANT !!!!!!!
+!!!!!! VERY IMPORTANT !!!!!!!
+*/
+
+static const char *kWifiSsid = "Your SSDI";
+static const char *kWifiPass = "Your Password";
+
+#endif
+
